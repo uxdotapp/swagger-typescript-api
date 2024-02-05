b0ed6d058e394f7fa0c1acbd7cf0c79b2e561069
Filip Bel <bel.filip@icloud.com>
2024-02-05 14:13:07 +0100

    fix: convert interface properties into camel case format

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const { cli } = require('./cli');
 const { generateApi, generateTemplates } = require('./src');
 const { HTTP_CLIENT } = require('./src/constants');
 const { resolve } = require('path');
+const _ = require("lodash");
 const { CodeGenConfig } = require('./src/configuration');
 const {
   TemplatesGenConfig,
@@ -18,6 +19,65 @@ const {
 
 const codeGenBaseConfig = new CodeGenConfig({});
 const templateGenBaseConfig = new TemplatesGenConfig({});
+
+function onCreateComponent(component) {
+  function mapEntries(object, mapCallbackFn) {
+    return Object.fromEntries(Object.entries(object).map(mapCallbackFn));
+  }
+
+  function camelCaseProperties(object) {
+    if (Array.isArray(object)) {
+      return object.map(camelCaseProperties);
+    } else if (typeof object === 'object' && object !== null) {
+      if (object.properties) {
+        object.properties = camelCaseProperties(object.properties);
+      }
+
+      if (object.items) {
+        object.items = camelCaseProperties(object.items);
+      }
+
+      ['allOf', 'anyOf', 'oneOf'].forEach((keyword) => {
+        if (object[keyword]) {
+          object[keyword] = camelCaseProperties(object[keyword]);
+        }
+      });
+
+      if (
+        object.additionalProperties &&
+        typeof object.additionalProperties === 'object'
+      ) {
+        object.additionalProperties = camelCaseProperties(
+          object.additionalProperties
+        );
+      }
+
+      if (object.required && Array.isArray(object.required)) {
+        object.required = object.required.map(_.camelCase);
+      }
+
+      return mapEntries(object, ([key, value]) => {
+        return [
+          /**
+           * Transforming key into camel case format needs to be done like follow 
+           * because camelCase method from lodash removes special characters while 
+           * performing transformation into camel case format.
+           * e.g., $ref key, that is introduced in OpenAPI 3.0, 
+           * will be converted into ref if camelCase method itself is used
+           * @see https://swagger.io/docs/specification/using-ref/
+           */
+          key.replace(/\w+/g, _.camelCase),
+          camelCaseProperties(value),
+        ]
+      });
+    } else {
+      return object;
+    }
+  }
+
+  component.rawTypeData = camelCaseProperties(component.rawTypeData);
+  return component;
+}
 
 const program = cli({
   name: name,
@@ -327,6 +387,9 @@ const main = async () => {
           httpClientType: options.axios ? HTTP_CLIENT.AXIOS : HTTP_CLIENT.FETCH,
           input: resolve(process.cwd(), options.path),
           output: resolve(process.cwd(), options.output || '.'),
+          hooks: {
+            onCreateComponent
+          },
           ...customConfig,
         });
         break;
